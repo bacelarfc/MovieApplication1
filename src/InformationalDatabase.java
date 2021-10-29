fc2cb8c9722dc737c75725cae7fa868a7646cc75
bacelarfc <fernandacbacelar@gmail.com>
2021-10-29 13:39:38 +0200

    Initial commit

---
+++ src/InformationalDatabase.java
@@ -0,0 +1,113 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class InformationalDatabase implements Serializable {
+
+    private ArrayList<Movie> movies;
+    private ArrayList<User> users;
+
+    public InformationalDatabase() {
+        this.movies = new ArrayList<>();
+        this.users = new ArrayList<>();
+       initializeMovieList();
+    }
+
+    public void addMovie(Movie movie) {
+        movies.add(movie);
+    }
+
+    public void initializeMovieList() {
+        Movie movie1 = new Movie("Find Nemo", 2003, "Dead parents, animation, fish");
+        Actor actor1ForMovie1 = new Actor("Ellen DeGeneres", "Dory");
+        Actor actor2ForMovie1 = new Actor("Albert Brookis", "Marlin");
+        movie1.addNewActor(actor1ForMovie1);
+        movie1.addNewActor(actor2ForMovie1);
+        movies.add(movie1);
+
+        Movie movie2 = new Movie("Harry Potter and the Philosopher’s Stone", 2001, "Dead parents, magic, glasses");
+        Actor actor1ForMovie2 = new Actor("Daniel Radcliff", "Harry Potter");
+        Actor actor2ForMovie2 = new Actor("Emma Watson", "Hermione Granger");
+        movie2.addNewActor(actor1ForMovie2);
+        movie2.addNewActor(actor2ForMovie2);
+        movies.add(movie2);
+
+        Movie movie3 = new Movie("Forrest Gump", 1994, "Vietnam War, smiley");
+        Actor actor1ForMovie3 = new Actor("Tom Hanks", "Forrest Gump");
+        Actor actor2ForMovie3 = new Actor("Robin Wright", "Jenny Curran");
+        movie3.addNewActor(actor1ForMovie3);
+        movie3.addNewActor(actor2ForMovie3);
+        movies.add(movie3);
+
+        Movie movie4 = new Movie("The Shawshank Redemption", 1994, "Prison, freedom, god");
+        Actor actor1ForMovie4 = new Actor("Tim Robbins", "Andy Dufresne");
+        Actor actor2ForMovie4 = new Actor("Morgan Freeman", "Ellis Boyd 'Red' Redding");
+        movie4.addNewActor(actor1ForMovie4);
+        movie4.addNewActor(actor2ForMovie4);
+        movies.add(movie4);
+
+        Movie movie5 = new Movie("The Fast and Furious: Tokyo Drift", 2006, "Cars, drifting, Japan, dead, person");
+        Actor actor1ForMovie5 = new Actor("Lucas Black", "Sean Boswell");
+        Actor actor2ForMovie5 = new Actor("Sung Kang", "Han Lue");
+        movie5.addNewActor(actor1ForMovie5);
+        movie5.addNewActor(actor2ForMovie5);
+        movies.add(movie5);
+
+        Movie movie6 = new Movie("Bambi", 1942, "Dead parents, animals, sad");
+        Actor actor1ForMovie6 = new Actor("Hardie Albright", "Bambi");
+        Actor actor2ForMovie6 = new Actor("Stan Alexander", "Flower");
+        movie6.addNewActor(actor1ForMovie6);
+        movie6.addNewActor(actor2ForMovie6);
+        movies.add(movie6);
+
+        Movie movie7 = new Movie("Air Bud", 1997, "Dog, basketball, sad");
+        Actor actor1ForMovie7 = new Actor("Michael Jeter", "Norm Snively");
+        Actor actor2ForMovie7 = new Actor("Kevin Zegers", "Josh Framm");
+        movie7.addNewActor(actor1ForMovie7);
+        movie7.addNewActor(actor2ForMovie7);
+        movies.add(movie7);
+
+        Movie movie8 = new Movie("Parasite", 2019, "Capitalism, social inequality");
+        Actor actor1ForMovie8 = new Actor("Kang-Ho Song", "Ki Taek");
+        Actor actor2ForMovie8 = new Actor("Sun-Kyun Lee", "Dong Ik");
+        movie8.addNewActor(actor1ForMovie8);
+        movie8.addNewActor(actor2ForMovie8);
+        movies.add(movie8);
+
+        Movie movie9 = new Movie("Big Momma's House", 2000, "Fart, Momma");
+        Actor actor1ForMovie9 = new Actor("Martinn Lawrence", "Malcolm Turner/Big Momma");
+        Actor actor2ForMovie9 = new Actor("Nia Long", "Sherry Pierce");
+        movie9.addNewActor(actor1ForMovie9);
+        movie9.addNewActor(actor2ForMovie9);
+        movies.add(movie9);
+
+        Movie movie10 = new Movie("Hot Fuzz", 2007, "British, cops");
+        Actor actor1ForMovie10 = new Actor("Simon Pegg", "Nicholas Angel");
+        Actor actor2ForMovie10 = new Actor("Nick Frost", "PC Danny Butterman");
+        movie10.addNewActor(actor1ForMovie10);
+        movie10.addNewActor(actor2ForMovie10);
+        movies.add(movie10);
+
+    }
+
+    // Method to add users to a userlist, adding the user
+    // will happen upon registration or by reading all users from an existing file (user-data.txt)
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public ArrayList<Movie> getMovies() {
+        return movies;
+    }
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    @Override
+    public String toString() {
+        return "InformationalDatabase{" +
+                "movies=" + movies +
+                ", users=" + users +
+                '}';
+    }
+}
---
+++ src/MovieApplication.java
@@ -0,0 +1,326 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+//when to use serialize
+
+public class MovieApplication implements Serializable {
+
+
+    private Scanner inputScanner;
+    private static final String USER_FILE_NAME = "user-data.txt";
+    private InformationalDatabase database;
+    private User activeUser;
+
+    public MovieApplication(InformationalDatabase iDatabase) {
+        this.inputScanner = new Scanner(System.in);
+        this.database = iDatabase;
+        this.activeUser = new User();
+        readUsersFromFile();
+    }
+
+    private String getUserFullLineInput() {
+        String userInput = inputScanner.nextLine();
+        userInput = inputScanner.nextLine();
+        return userInput;
+    }
+
+    public void displayMenu() {
+        System.out.println("--- MOVIE APP MENU ---");
+        System.out.println("Press 1 to search for a movie");
+        System.out.println("Press 2 to see the account statistics");
+        System.out.println("Press 3 to save a movie as favorite");
+        System.out.println("Press 4 to list all favorite movies");
+        System.out.println("Press 5 to quit");
+        System.out.println("---------------------");
+    }
+
+    public void askForCredentials() {
+        System.out.println("Are you a registered user or not (Y/N) ?");
+        String userChoice = inputScanner.next();
+        while (!userChoice.equalsIgnoreCase("Y") && !userChoice.equalsIgnoreCase("N")) {
+            System.out.println("--- ERROR: Invalid input, please try again!");
+            System.out.println("Are you a registered user or not (Y/N) ?");
+            userChoice = inputScanner.next();
+        }
+        if (userChoice.equalsIgnoreCase("y")) {
+            login();
+        } else if (userChoice.equalsIgnoreCase("n")) {
+            registerNewUser();
+        }
+    }
+
+    public void login() {
+        System.out.print("Please input your email: --> ");
+        String email = inputScanner.next();
+        System.out.print("Please input your password: --> ");
+        String password = inputScanner.next();
+        System.out.println();
+        boolean foundUser = false;
+        for (User user : database.getUsers()) {
+            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
+                activeUser = user;
+                foundUser = true;
+            }
+        }
+        if (foundUser == true) {
+            System.out.println(" --- GLAD TO SEE YOU BACK, " + activeUser.getName() + " ---");
+        } else {
+            System.out.println(" --- EMAIL OR PASSWORD DO NOT MATCH, TRY AGAIN ---");
+            login();
+        }
+    }
+
+    public void registerNewUser() {
+        System.out.print("Please input your name: --> ");
+        String name = inputScanner.nextLine();
+        name = inputScanner.nextLine();
+        System.out.println();
+        System.out.print("Please input your email: --> ");
+        String email = inputScanner.next();
+        System.out.print("Please input your password: --> ");
+        String password = inputScanner.next();
+        System.out.println();
+        User user = new User(name, email, password);
+        //change
+        if (checkIfUniqueUser(email) == true) {
+            System.out.println(" --- SORRY, THIS EMAIL IS ALREADY TAKEN! --- ");
+        } else {
+            saveUserToFile(user);
+            database.addUser(user);
+        }
+
+    }
+
+    public boolean checkIfUniqueUser(String email) {
+        if (database.getUsers().size() == 0) {
+            return true;
+        }
+        for (User user : database.getUsers()) {
+            if (user.getEmail().equals(email)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void saveUserToFile(User user) {
+        try {
+            FileWriter fileWriter = new FileWriter(USER_FILE_NAME, true);
+            String userInfo = user.getName() + "," + user.getEmail() + "," + user.getPassword() + "\n";
+            fileWriter.write(userInfo);
+            fileWriter.close();
+        } catch (Exception exception) {
+            System.out.println("ERROR: wrong file name or missing file!");
+        }
+    }
+
+    public void readUsersFromFile() {
+        try {
+            File file = new File(USER_FILE_NAME);
+            if (file.exists()) {
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+                String line = "";
+                while ((line = bufferedReader.readLine()) != null) {
+                    String[] info = line.split(",");
+                    String name = info[0];
+                    String email = info[1];
+                    String password = info[2];
+                    User user = new User(name, email, password);
+                    database.addUser(user);
+                }
+            } else {
+                System.out.println("FILE DOESNT EXIST");
+            }
+
+        } catch (Exception exception) {
+            System.out.println("ERROR: wrong file name or missing file!");
+        }
+    }
+
+    public void run() {
+        System.out.println("----- Welcome to the MOVIE APPLICATION -----");
+        askForCredentials();
+        int userMenuChoice = -1;
+        while (userMenuChoice != 5) {
+            displayMenu();
+            userMenuChoice = inputScanner.nextInt();
+            if (userMenuChoice == 1) {
+                searchForMovie();
+            }
+            if (userMenuChoice == 2) {
+                displayAccountStatistics();
+            }
+            if (userMenuChoice == 3) {
+                saveMovieAsFavorite();
+            }
+            if (userMenuChoice == 4) {
+                listAllFavoriteMovies();
+            }
+            if (userMenuChoice == 5) {
+                System.out.println("--- SAD TO SEE YOU GO ---");
+                break;
+            }
+        }
+    }
+
+    public void searchForMovie() {
+        System.out.println("Pick one criteria to search by");
+        System.out.println("Press 1 for searching by title");
+        System.out.println("Press 2 for searching by production year");
+        System.out.println("Press 3 for searching by an actor");
+        int userChoice = inputScanner.nextInt();
+        switch (userChoice) {
+            case 1:
+                searchByTitle();
+                break;
+            case 2:
+                searchByProductionYear();
+                break;
+            case 3:
+                searchByActor();
+                break;
+            default:
+                System.out.println(" *** INVALID INPUT, RETURNING TO MAIN MENU ***");
+                break;
+        }
+    }
+
+
+    private void processMovieResult(ArrayList<Movie> movieArrayList) {
+        System.out.println(" --- SEARCH RETURNED " + movieArrayList.size() + " RESULT: --- ");
+        for (Movie movie : movieArrayList) {
+            System.out.println(movie.toString());
+            System.out.println("---------------------");
+        }
+
+        if (movieArrayList.size() > 0) {
+            System.out.println("Press 1 to play a movie");
+            System.out.println("Press 2 to add movie to favourites");
+            System.out.println("Press 3 to return to main menu");
+
+            int userChoice = inputScanner.nextInt();
+           /* System.out.println("Input the title of the movie >> ");
+            String title = getUserFullLineInput();*/
+
+            Movie movieChoice = null;
+
+            for (Movie movie : movieArrayList) {
+                //if (movie.getTitle().equalsIgnoreCase(title)) {
+                    movieChoice = movie;
+               // }
+
+            }
+
+            if (movieChoice == null) {
+                System.out.println(" *** SEARCH DIDN'T RETURN ANY RESULT, RETURNING TO MAIN MENU *** ");
+                return;
+            }
+
+            if (userChoice == 1) {
+                System.out.println("..... PLAYING A MOVIE '" + movieChoice.getTitle() + "' .....");
+                ArrayList<Actor> actorArrayList = movieChoice.getActors();
+                System.out.println("................... ACTORS .......................");
+                for (Actor actor : actorArrayList) {
+                    System.out.println(actor.toString());
+                }
+                System.out.println("..................................................");
+                return;
+            } else if (userChoice == 2) {
+                System.out.println("..... ADDING THE MOVIE TO THE FAV LIST .....");
+                // add this info to a file ?
+                activeUser.getFavMovieList().add(movieChoice);
+            } else if (userChoice == 3) {
+                return;
+            }
+        }
+
+
+    }
+
+    private void searchByTitle() {
+        System.out.println("Input a title of the movie: >> ");
+        String title = getUserFullLineInput();
+        for (Movie movie : database.getMovies()) {
+            if (movie.getTitle().equals(title)) {
+                ArrayList<Movie> resultList = new ArrayList<>();
+                resultList.add(movie);
+                processMovieResult(resultList);
+                return;
+            }
+        }
+        System.out.println(" *** SEARCH DIDN'T RETURN ANY RESULT, RETURNING TO MAIN MENU *** ");
+    }
+
+
+    private void searchByProductionYear() {
+        System.out.println("Input a production year of the movie: >> ");
+        int prodYear = inputScanner.nextInt();
+        ArrayList<Movie> resultList = new ArrayList<>();
+
+        for (Movie movie : database.getMovies()) {
+            if (movie.getProdYear() == (prodYear)) {
+                resultList.add(movie);
+            }
+        }
+        processMovieResult(resultList);
+
+    }
+
+    private void searchByActor() {
+        System.out.println("Input an actor name to search a movie by: >> ");
+        String actorName = getUserFullLineInput();
+        ArrayList<Movie> resultList = new ArrayList<>();
+        for (Movie movie : database.getMovies()) {
+            ArrayList<Actor> actorArrayList = movie.getActors();
+            for (Actor actor : actorArrayList) {
+                if (actor.getFullName().equals(actorName)) {
+                    resultList.add(movie);
+                }
+            }
+        }
+        processMovieResult(resultList);
+
+    }
+
+
+    public void displayAccountStatistics() {
+
+    }
+
+    public void saveMovieAsFavorite() {
+
+    }
+
+    public void listAllFavoriteMovies() {
+
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        InformationalDatabase database = new InformationalDatabase();
+        File file = new File("database.ser");
+
+        database.initializeMovieList();
+
+       /* FileOutputStream fos = new FileOutputStream(file);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(database);
+        oos.flush();
+        fos.close();
+        oos.close();*/
+
+
+        //deserialize
+        FileInputStream fis = new FileInputStream(file);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+
+
+
+        InformationalDatabase iDatabase = (InformationalDatabase) ois.readObject();
+        fis.close();
+        ois.close();
+
+        MovieApplication movieApplication = new MovieApplication(iDatabase);
+        movieApplication.run();
+
+        }
+    }
