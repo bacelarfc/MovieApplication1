2aaf8fef7c856214addb56f0657ec75f55400369
mubibic <amal487d@stud.kea.dk>
2021-10-31 13:04:08 +0100

    øfrick

--- src/Statistics.java
+++ src/Statistics.java
@@ -1,3 +1,5 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Statistics {
 
     private InformationalDatabase iDatabase;
@@ -14,10 +16,14 @@ public class Statistics {
         //print movie and get the title maybe also get the movie history name and the dates stuff, use the parameters there ???? ufck
 
         System.out.println();
-
+        System.out.println("-----------------------");
+        System.out.println("M O V I E   H I S T O R Y");
         for(MovieHistory movieRecord : activeUser.getMovieHistory()){
-            System.out.println("The movie " + movieRecord.getMovie().getTitle() + "was played on " + movieRecord.getDatePlayed()+ "by " + activeUser.getName());
+
+            System.out.println("The movie " + movieRecord.getMovie().getTitle() + " was played on " + movieRecord.getDatePlayed()+ " by " + activeUser.getName());
+
         }
+        System.out.println("-----------------------");
 
     }
 
